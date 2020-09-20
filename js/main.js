38696c283f47a4ce0d8c9801d106f9b04b1ce61b
alejandro599 <josealejandrocharcas@gmail.com>
2020-09-20 01:58:50 -0300

    Paleta

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('color').addEventListener('input', getColor)
+
+function getColor(){
+    let col = document.getElementById('color').value 
+
+    document.getElementById('visualizador').style.background=col
+    document.getElementById('visualizador').innerHTML=col
+}
